650426fa4d79b56d5cac6fd69ae06640fda82a90
Satyam Kumar <satyamkumar@Satyams-MacBook-Air.local>
2025-07-18 11:46:14 +0530

    Updated requirements.txt and fixed gunicorn issue

--- app.py
+++ app.py
@@ -14,5 +14,8 @@ def chat():
     response = get_ai_response(user_message, personality)
     return jsonify({'response': response})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
